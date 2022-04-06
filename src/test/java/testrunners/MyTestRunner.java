ae3999b4165b71e045a764d1968ad1f8064a1cc4
ShivamG56 <gulati.shivam2008@gmail.com>
2022-04-06 06:58:03 +0530

    reporting added

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/AppFeatures"},
         glue = {"stepdefinitions", "AppHooks"},
-        plugin = {"pretty"
+        plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 
 
         }
